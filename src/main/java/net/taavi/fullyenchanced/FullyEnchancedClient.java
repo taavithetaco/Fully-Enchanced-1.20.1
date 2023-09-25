ed0f70221b51dc83d6287d0482bcc1eb4cec9e5e
Taavi <taavidinneen@gmail.com>
2023-09-25 17:36:47 -0400

    logger

---
+++ src/main/java/net/taavi/fullyenchanced/FullyEnchancedClient.java
@@ -0,0 +1,10 @@
+package net.taavi.fullyenchanced;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class FullyEnchancedClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/taavi/fullyenchanced/FullyEnchancedDataGenerator.java
@@ -0,0 +1,11 @@
+package net.taavi.fullyenchanced;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class FullyEnchancedDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
